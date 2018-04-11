ca38e933f4dd43fc3fdcc4db27564b5e1559edf1
Andrew Low <andrew.low@inspection.gc.ca>
2018-04-11 15:38:51 -0400

    Increment version to 0.2.1 - now with primer3 function

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sigseekr",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     scripts=['sigseekr/sigseekr.py'],
     author="Andrew Low",
